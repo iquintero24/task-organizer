408282a61f487ee66d42ccb9f9b3df01e8e213ac
iquintero24 <i.quintero@civitran.com>
2025-07-03 00:19:08 -0500

    feat: tasks filtered by status

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,21 @@
-/** app.js es la conexion entre los servicios de la aplicacion 
- * y el dom aca se maneja la logica de los services y el dom.
-*/
-
-import "./dom.js"
-import { renderTasks } from "./dom.js";
 import { getData } from "./service.js";
+import { renderTasks } from "./dom.js";
+
+const containerPending = document.getElementById("container_task_pending");
+const containerHold = document.getElementById("container_task_hold");
+const containerprogress = document.getElementById("container_task_progress")
+const containercompleted = document.getElementById("container_task_completed")
 
 window.onload = async function () {
   const tasks = await getData();
-  renderTasks(tasks)// esta función accede al DOM
+
+  const pendingTasks = tasks.filter(task => task.status === "pending");
+  const holdTasks = tasks.filter(task => task.status === "on-hold");
+  const progressTasks = tasks.filter(task => task.status === "in-progress")
+  const completedTasks = tasks.filter(task => task.status === "completed")
+
+  renderTasks(pendingTasks, containerPending);
+  renderTasks(holdTasks, containerHold);
+  renderTasks(progressTasks, containerprogress);
+  renderTasks(completedTasks, containercompleted);
 };
--- src/js/service.js
+++ src/js/service.js
@@ -10,8 +10,8 @@ export async function getData() {
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-
     const json = await response.json();
+    console.log(json);
     return json;
   } catch (error) {
     console.error(error.message);
